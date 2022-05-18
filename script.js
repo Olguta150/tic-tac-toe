e6530a290fbbc7e66c66393fee29c469ab0d636a
Olguta Fratila <fratilaolguta150@gmail.com>
2022-05-18 22:45:05 +0300

    Add names of the players, allow players to add marks to a specific spot on board, put winning conditions, make the game restart

--- script.js
+++ script.js
@@ -1,42 +1,115 @@
 // Setting up the gameBoard module
 const gameBoardModule = (function() {
-    const gameBoard = ['O', 'X', 'O', 'X', 'X', 'O', 'X', 'O', 'X'];
-    return{gameBoard};
-})();
-
-// Setting up the displayController module
-const displayControllerModule = (function() {
-    let testF = () => { console.log('testing a private function call inside of a module object') }
-    return {testF};
+    let gameBoard = ['', '', '', '', '', '', '', '', ''];
+    return {gameBoard};
 })();
 
 // Setting up the player factory function
-const createPlayer = (playerName, playerNumber, assignedXO) => {
-    let getPlayerName = () => {
-        playerName;
-        console.log(`This is the name of player ${playerNumber}: ${playerName}`);
+const createPlayer = (playerName) => {
+    let currentPlayer = 'O';
+    myGameNames = [];
+
+    function addNamesToGame() {
+        const playerOneName = document.getElementById('add-player1-name');
+        const playerTwoName = document.getElementById('add-player2-name');
+        let playerOne = playerOneName.value;
+        let playerTwo = playerTwoName.value;
+        myGameNames.push(playerOne);
+        myGameNames.push(playerTwo);
     }
-    return {getPlayerName, playerName, playerNumber, assignedXO};
+
+    function displayName() {
+        const getPlayer1 = document.querySelector('.get-player1');
+        const getPlayer2 = document.querySelector('.get-player2');
+        getPlayer1.innerHTML = myGameNames[0];
+        getPlayer2.innerHTML = myGameNames[1];
+    }
+
+    function displaySecondLayout() {
+        const layoutTwo = document.querySelector('.layout-2');
+        const layoutOne = document.querySelector('.layout-1');
+        const title = document.querySelector('h1');
+
+        layoutTwo.style.display = 'flex';
+        layoutOne.style.display = 'none';
+        title.style.display = 'none';
+    }
+
+    document.querySelector('.start-button').addEventListener('click', (e) => {
+        addNamesToGame();
+        displayName();
+        displaySecondLayout();
+    })
+
+    function cellHandlerClick(cellClickTarget) {
+        const cellClick = cellClickTarget.target;
+        
+        const cellClickIndex = parseInt(cellClick.getAttribute('data-cell-index'));
+        
+        if(gameBoardModule.gameBoard[cellClickIndex] !== '') {
+            return;
+        }
+
+            cellClickAssign(cellClick, cellClickIndex);
+            handleResultVariation();
+    }
+
+    function cellClickAssign(cellClick, cellClickIndex) {
+        gameBoardModule.gameBoard[cellClickIndex] = currentPlayer;
+        cellClick.innerHTML = currentPlayer;
+    }
+
+    const winningConditions = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
+    function handleResultVariation() {
+        const scoreFirstPlayer = document.querySelector('.score-player1');
+        const scoreSecondPlayer = document.querySelector('.score-player2');
+        scoreFirstPlayer.textContent = 0;
+        scoreSecondPlayer.textContent = 0;
+        let roundWon = false;
+        for(i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            let a = gameBoardModule.gameBoard[winCondition[0]];
+            let b = gameBoardModule.gameBoard[winCondition[1]];
+            let c = gameBoardModule.gameBoard[winCondition[2]];
+            if(a === '' || b === '' || c === '') {
+                continue;
+            }
+            if(a === b && b === c) {
+                roundWon = true;
+                break;
+            }
+        }
+        if(roundWon) {
+            if(currentPlayer === 'X') {
+                scoreFirstPlayer.textContent = 'won';
+            } else {
+                scoreSecondPlayer.textContent = 'won';
+            }
+        }
+
+        let roundDraw = !gameBoardModule.gameBoard.includes('');
+        if(roundDraw) {
+            document.querySelector('.get-name-container').style.display = 'none';
+            document.querySelector('.it-is-a-draw').style.display = 'block';
+        }
+
+        handlePlayerChange();
+    }
+
+    handleResultVariation();
+
+    function handlePlayerChange() {
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+
+    document.querySelectorAll('.bloc').forEach(cell => cell.addEventListener('click', cellHandlerClick));
+
+    document.querySelector('.restart-button').addEventListener('click', () => {
+        location.reload();
+    })
+
+    return { playerName };
 }
 
-const renderGameBoardContent = (() => {
-    const allBloc = document.querySelectorAll('.bloc');
-    for(i = 0; i < gameBoardModule.gameBoard.length; i++) {
-        allBloc[i].textContent = gameBoardModule.gameBoard[i];
-        console.log('show array content', gameBoardModule.gameBoard[i]);
-    }
-    // return{};
-})();
 
-
-// make a function which takes two parameters, X and O, which on the first time use X and for the following time use O
-// check if the spot is empty or not
-// for the first click store X in library, renderGameBoardContent
-//for the second click store O in library, renderGameBoardContent
-
-
-
-let Roua = createPlayer('Roua', 1, 'X');
-let Rambo = createPlayer('Rambo', 2, 'O');
-
-// when restart the game, should set the array to [];
+let Roua = createPlayer('Roua');
